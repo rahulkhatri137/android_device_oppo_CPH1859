dfba05c1b4a20ba0fdfec007b5c19a72ed87235c
rahulkhatri137 <rahulkhatri137@gmail.com>
2022-11-03 16:25:44 +0000

    CPH1859: Bringup AEX
    Signed-off-by: rahulkhatri137 <rahulkhatri137@gmail.com>

---
+++ aosp_CPH1859.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2021-2022 The AEX Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit from CPH1859 device
+$(call inherit-product, device/oppo/CPH1859/device.mk)
+
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+# Gapps
+WITH_GAPPS := true
+$(call inherit-product, vendor/google/common/common-vendor.mk)
+
+# AOSP Stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_BLUR := true
+TARGET_SHIP_GCAM_GO := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_FACE_UNLOCK := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_CPH1859
+PRODUCT_DEVICE := CPH1859
+PRODUCT_BRAND := oppo
+PRODUCT_MODEL := realme 1
+PRODUCT_MANUFACTURER := oppo
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_oppo6771_17065-user 9 PPR1.180610.011 eng.root.20200509.060522 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:12/SQ3A.220705.003.A1/8672226:user/release-keys
