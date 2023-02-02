07526c600a52d7cbc2c5a5d009f38b900c772cbb
rahulkhatri137 <rahulkhatri137@gmail.com>
2023-02-02 14:10:54 +0000

    CPH1859: xdroid Bringup
    Signed-off-by: rahulkhatri137 <rahulkhatri137@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2022-2023 The xdroidOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_CPH1859.mk
+    $(LOCAL_DIR)/xdroid_CPH1859.mk
