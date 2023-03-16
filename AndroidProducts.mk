ed3015ea7aa6cb350e1404022398a80402f1e669
rahulkhatri137 <rahulkhatri137@gmail.com>
2023-03-16 05:41:41 +0000

    CPh1859: Cipher-fy
    Signed-off-by: rahulkhatri137 <rahulkhatri137@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 #
-# Copyright (C) 2022-2023 The xdroidOS Project
+# Copyright (C) 2022-2023 The CipherOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xdroid_CPH1859.mk
+    $(LOCAL_DIR)/cipher_CPH1859.mk
